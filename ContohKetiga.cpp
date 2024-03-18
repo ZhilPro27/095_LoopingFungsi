42657314aaad51d7b01a97b3042d642bddbb5099
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-18 14:57:57 +0700

    Looping menggunakan DoWhile

---
+++ ContohKetiga.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char pilihan;
+
+    do{
+        cout << "Berangkat mengambil takjil" << endl;
+        cout << "Antri takjil" << endl;
+        cout << "Mengambil takjil" << endl;
+        cout << "Apakah mau antri kembali?" << endl;
+        cin >> pilihan;
+    }while(pilihan == 'y' || pilihan == 'Y');
+}
